0df26685f831fbeac78681c6b16aac6ad8395bfd
HiuFung Kwok <hiufkwok@gmail.com>
2023-11-07 17:18:32 +0800

    SPARK-45522: Updat ssl server changes

--- core/src/main/scala/org/apache/spark/ui/JettyUtils.scala
+++ core/src/main/scala/org/apache/spark/ui/JettyUtils.scala
@@ -315,7 +315,7 @@ private[spark] object JettyUtils extends Logging {
       httpConfig.setRequestHeaderSize(requestHeaderSize)
 
       // If SSL is configured, create the secure connector first.
-      val securePort = sslOptions.createJettySslContextFactory().map { factory =>
+      val securePort = sslOptions.createJettySslContextFactoryServer().map { factory =>
 
         val securePort = sslOptions.port.getOrElse(if (port > 0) Utils.userPort(port, 400) else 0)
         val secureServerName = if (serverName.nonEmpty) s"$serverName (HTTPS)" else serverName
